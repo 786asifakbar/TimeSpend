54b722956fe867b41e50ca2c80cfbfc3c9ff82d2
Asif Akbar <786asifakbar53@gmail.com>
2026-01-25 19:52:13 +0500

    pages created

--- timespend-frontend/src/App.jsx
+++ timespend-frontend/src/App.jsx
@@ -7,19 +7,21 @@ import Expenses from "./pages/Expenses.jsx";
 import Reports from "./pages/Reports.jsx";
 import { useAuth } from "./context/AuthContext.jsx";
 import TimeLogs from "./pages/TimeLogs";
+import Admin from "./pages/Admin.jsx";
 
 
 
 
 const App = () => {
   const { user } = useAuth();
-
   return (
     <Routes>
       <Route path="/" element={user ? <Navigate to="/dashboard" /> : <Login />} />
       <Route path="/register" element={<Register />} />
 
       <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/" />} />
+      <Route path="/admin" element={user ? <Admin /> : <Navigate to="/" />} />
+      <Route path="/home" element={user ? <Expenses /> : <Navigate to="/" />} />
       <Route path="/expenses" element={user ? <Expenses /> : <Navigate to="/" />} />
       <Route path="/reports" element={user ? <Reports /> : <Navigate to="/" />} />
       <Route path="/timelogs" element={user ? <TimeLogs /> : <Navigate to="/" />}
